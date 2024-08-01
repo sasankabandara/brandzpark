fa772532bfc54fd47a732962c48853a9f1cf3286
Sasanka <sasankabandara28@gmail.com>
2024-08-01 18:47:19 +0530

    small screen fix

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -2,33 +2,31 @@ import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import NavBar from "../NavBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../NavBar/nav.css";
-import "../Banner/banner.css"
+
+import "../Banner/banner.css";
 
 const Banner = () => {
   return (
     <div>
       <NavBar />
       <div
-        className="banner-background vh-100 d-flex justify-content-center align-items-center text-white "
-        style={{ backgroundImage: "url(/assets/banner1.jpeg)", backgroundSize: "cover" ,backgroundPosition: "center " }}
+        className="banner-background vh-100 d-flex justify-content-center align-items-center text-white"
+        style={{ backgroundImage: "url(/assets/banner1.jpeg)" }}
       >
-        <Container className="p-4 banner-container" >
+        <Container className="p-4 banner-container">
           <Row className="align-items-center">
             <Col md={6} className="text-left">
-              <div className="cont" style={{ marginLeft: "250px", marginTop: "20px" }}>
-                <h1 className="display-4 text-left" style={{ fontWeight: "400", fontSize: "6.3rem",lineHeight: "0.9" }}>
-                  Let’s
-                  <br /> 
+              <div className="cont">
+                <h1 className="display-4 text-left banner-title">
+                  Let's
+                  <br />
                   <span style={{ color: "#E6320F" }}>Spark </span>
-                  
                   Your Brand
                 </h1>
-
-                <Button variant="warning" className="mt-5">
+                <Button className="custom-button"  style={{ marginTop:"100px" }} >
                   BOOK A CALL
                 </Button>
-                <p className="lead">
+                <p className="lead mt-3" style={{ fontSize:"15px" }}>
                   To empower brands with creativity and strategy, enabling them
                   to reach their full potential and thrive in the competitive
                   landscape.
